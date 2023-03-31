469983e1d38b9437c108317e3fb371698bc4e71b
VitalyChopik <leo.space97@gmail.com>
2023-03-31 17:56:31 +0300

    feat: добавил добавление социальных сетей

--- haggberg/inc/enqueue-scripts.php
+++ haggberg/inc/enqueue-scripts.php
@@ -7,6 +7,7 @@ function px_site_scripts()
 
 	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/css/style.min.css' );
   wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/app.min.js', array(), null, true );
+	wp_dequeue_style( 'wp-block-library' );
 }
 
 add_action( 'wp_enqueue_scripts', 'px_site_scripts' );
