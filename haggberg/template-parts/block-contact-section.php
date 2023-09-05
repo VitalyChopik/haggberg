20abf988304a301a1b69e1e1398bbc1f8a96c0b7
VitalyChopik <leo.space97@gmail.com>
2023-09-05 12:57:21 +0300

    feat: сделана секция contact section

--- haggberg/template-parts/block-contact-section.php
+++ haggberg/template-parts/block-contact-section.php
@@ -12,7 +12,7 @@
 
 <div class="contact__section">
   <div class="contact__container">
-    <h1 class="page__title contact__title"><?php the_title();?></h1>
+    <h1 class="page__title contact__title"><?php the_field('title');?></h1>
     <div class="contact__info">
       <div class="contact__info-box">
         <div class="contact__info-title">Our phone number</div><a href="tel:<?php the_field('phone', 'option');?>"
@@ -23,12 +23,5 @@
           class="contact__info-link"><?php the_field('email', 'option');?></a>
       </div>
     </div>
-  </div>
-</div>
-<?php echo get_template_part( 'template-parts/content', 'cta', ['class'=>'contact__cta']); ?>
-<div class="contact__section form">
-  <div class="contact__container">
-    <h1 class="page__title contact__form-title"><?php the_field('title_form');?></h1>
-  <?php echo do_shortcode('[contact-form-7 id="153" title="Contact form 1" html_class="contact__form-block"]');?>
   </div>
 </div>
