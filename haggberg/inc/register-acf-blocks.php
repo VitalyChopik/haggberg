3ed794d829b399385eec470df6cb25d224161ca5
VitalyChopik <leo.space97@gmail.com>
2023-09-05 12:35:58 +0300

    feat: добавил блоки в БД

--- haggberg/inc/register-acf-blocks.php
+++ haggberg/inc/register-acf-blocks.php
@@ -16,6 +16,42 @@ function my_register_blocks() {
             'render_callback'   => 'my_acf_block_render_callback',
             'category'          => 'layout',
             ));
+        acf_register_block_type(array(
+            'name'            => 'case-section',
+            'title'             => __('Case Section'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'layout',
+            ));
+        acf_register_block_type(array(
+            'name'            => 'about',
+            'title'             => __('About'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'layout',
+            ));
+        acf_register_block_type(array(
+            'name'            => 'contact-section',
+            'title'             => __('Contact Section'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'layout',
+            ));  
+        acf_register_block_type(array(
+            'name'            => 'cta',
+            'title'             => __('cta'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'layout',
+            )); 
+        acf_register_block_type(array(
+            'name'            => 'service-block',
+            'title'             => __('Service Block'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'layout',
+            ));   
+        acf_register_block_type(array(
+            'name'            => 'service-section',
+            'title'             => __('Service Section'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'layout',
+            ));   
     }
 }
 
