e0c2b803e01036fe66144656457dd555b47d8a74
VitalyChopik <leo.space97@gmail.com>
2023-09-05 17:39:29 +0300

    feat: подкорректировал основные файлы

--- haggberg/template-parts/project-case.php
+++ haggberg/template-parts/project-case.php
@@ -1,13 +1,16 @@
-<?php global $projectNumber?>
+<?php $projectNumber = $args['count'];
+?>
 <article class="project__post">
   <div class="project__post-text">
     <h2 class="project__post-title"><?php if($projectNumber<10) { ?>0<?php echo $projectNumber; }else { echo $projectNumber; } ?>. <?php the_title();?></h2>
-    <!-- <div class="project__post-data">
-      <div class="project__post-location">
-        <img src="echo get_template_directory_uri()/images/icons/location.svg" alt="" class="project__post-icon">
-        <span class="project__post-city"> the_field('city')</span>
-      </div>
-    </div> -->
+  <?php
+    // <div class="project__post-data">
+    //   <div class="project__post-location">
+    //     <img src="echo get_template_directory_uri()/images/icons/location.svg" alt="" class="project__post-icon">
+    //     <span class="project__post-city"> the_field('city')</span>
+    //   </div>
+    // </div>
+  ?>
   </div>
   <a href="<?php the_permalink();?>" class="project__post-image">
     <?php the_post_thumbnail('full', ['class' => "project__post-thumbnails"]);?>
