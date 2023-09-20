95812566fbb883a9d76510fb1fdb2246b485d431
VitalyChopik <leo.space97@gmail.com>
2023-09-20 19:42:24 +0300

    feat: исправил блок cta

--- template-parts/block/block-cta.php
+++ template-parts/block/block-cta.php
@@ -27,23 +27,9 @@
          $image = get_sub_field('image');
           echo wp_get_attachment_image( $image, 'full', false, ['class'=> 'cta__img']);?>
         </div>
-        <?php 
-        if(get_sub_field('text_btn')){
-          ?>
-          <?php if( have_rows('big_title') ): ?>
-            <div class="cta__big-text">
-            <?php while( have_rows('big_title') ): the_row(); ?>
-            <span><?php the_sub_field('title');?></span>
-            <?php endwhile; ?>
-            </div>
-          <?php endif; ?>
-          <?php
-        } else {
-          ?>
-            <span class="cta__text"><?php the_sub_field('small_text');?></span>
-          <?php
-        }
-        ?>
+        <div class="cta__block-text">
+          <?php the_sub_field('text');?>
+        </div>
       </div>
     </div>
   </div>
