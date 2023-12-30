74d48690254a4c1ca38adae457abb8648e57919f
VitalyChopik <leo.space97@gmail.com>
2023-12-30 19:39:13 +0300

    fix: style

--- about.php
+++ about.php
@@ -5,13 +5,10 @@ Template Post Type: page
 */
 get_header();
 ?>
-<div class="about__section">
-  <div class="about__container">
-    <h1 class="page__title about__title"><?php the_title();?></h1>
-  </div>
-</div>
+
 <div class="about__section page__content">
   <div class="about__container">
+  <h1 class="page__title about__title"><?php the_title();?></h1>
     <?php the_content();?>
   </div>
 </div>
